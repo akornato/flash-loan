fe297b1d82ea13b4608d078746e97590e869f19e
Artur Kornatowski <artur.kornatowski@gmail.com>
2022-04-19 15:44:05 +0200

    add impersonatedTokenTransfer

--- scripts/flash-loan.ts
+++ scripts/flash-loan.ts
@@ -1,28 +1,42 @@
 import { ethers, network } from "hardhat";
+import type { BigNumber } from "ethers";
 
-const poolAddressProviderAddress = "0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb";
-const daiTokenAddress = "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1";
-const daiTokenHolder = "0x0C249eF4592869a6bF8195d90de948BE2b7c2744";
-
-async function main() {
-  const FlashLoan = await ethers.getContractFactory("FlashLoan");
-  const flashLoan = await FlashLoan.deploy();
-  await flashLoan.deployed();
-
+const impersonatedTokenTransfer = async ({
+  token,
+  from,
+  to,
+  amount,
+}: {
+  token: string;
+  from: string;
+  to: string;
+  amount: BigNumber;
+}) => {
   await network.provider.request({
     method: "hardhat_impersonateAccount",
-    params: [daiTokenHolder],
+    params: [from],
   });
-  const impersonatedSigner = await ethers.getSigner(daiTokenHolder);
-  const token = await ethers.getContractAt("IERC20", daiTokenAddress);
-  const txTransfer = await token
+  const impersonatedSigner = await ethers.getSigner(from);
+  const tokenContract = await ethers.getContractAt("IERC20", token);
+  const txTransfer = await tokenContract
     .connect(impersonatedSigner)
-    .transfer(flashLoan.address, 500000000000000); // premium
+    .transfer(to, amount);
   await txTransfer.wait();
   await network.provider.request({
     method: "hardhat_stopImpersonatingAccount",
-    params: [daiTokenHolder],
+    params: [from],
   });
+};
+
+async function main() {
+  const poolAddressProviderAddress =
+    "0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb";
+  const daiTokenAddress = "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1";
+  const daiTokenHolder = "0x0C249eF4592869a6bF8195d90de948BE2b7c2744";
+
+  const FlashLoan = await ethers.getContractFactory("FlashLoan");
+  const flashLoan = await FlashLoan.deploy();
+  await flashLoan.deployed();
 
   const poolAddressProvider = await ethers.getContractAt(
     "IPoolAddressesProvider",
@@ -30,8 +44,17 @@ async function main() {
   );
 
   const poolAddress = await poolAddressProvider.getPool();
-
   const pool = await ethers.getContractAt("IPool", poolAddress);
+  const premium = await pool.FLASHLOAN_PREMIUM_TOTAL();
+  // contrary to Aave documentation, FLASHLOAN_PREMIUM_TOTAL is expressed as a fraction of 10000 rather than percentages
+  const premiumAmount = ethers.utils.parseEther("1").mul(premium).div(10000);
+
+  await impersonatedTokenTransfer({
+    token: daiTokenAddress,
+    from: daiTokenHolder,
+    to: flashLoan.address,
+    amount: premiumAmount,
+  });
 
   const paramsEncoded = ethers.utils.defaultAbiCoder.encode(
     ["address", "uint256"],
