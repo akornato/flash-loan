317da00245ed08be0951b5a759686f715436383f
Artur Kornatowski <artur.kornatowski@gmail.com>
2023-12-09 06:47:55 +0100

    Removed Aave references from project files

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -3,8 +3,6 @@ pragma solidity 0.8.10;
 
 import "hardhat/console.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
-import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
-import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
 import {TransferHelper} from "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
@@ -17,98 +15,7 @@ contract FlashLoan {
 
     ISwapRouter private swapRouter;
 
-    function swapExactInputSingle(
-        address tokenIn,
-        address tokenOut,
-        uint256 amountIn
-    ) private returns (uint256 amountOut) {
-        TransferHelper.safeApprove(tokenIn, address(swapRouter), amountIn);
+    // Removed Aave references
 
-        // Naively set amountOutMinimum to 0. In production, use an oracle or other data source to choose a safer value for amountOutMinimum.
-        // We also set the sqrtPriceLimitx96 to be 0 to ensure we swap our exact input amount.
-        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
-            .ExactInputSingleParams({
-                tokenIn: tokenIn,
-                tokenOut: tokenOut,
-                fee: 3000,
-                recipient: address(this),
-                deadline: block.timestamp,
-                amountIn: amountIn,
-                amountOutMinimum: 0,
-                sqrtPriceLimitX96: 0
-            });
-
-        // The call to `exactInputSingle` executes the swap.
-        amountOut = swapRouter.exactInputSingle(params);
-    }
-
-    /**
-     * @notice Executes an operation after receiving the flash-borrowed asset
-     * @dev Ensure that the contract can return the debt + premium, e.g., has
-     *      enough funds to repay and has approved the Pool to pull the total amount
-     * @param borrowedToken The address of the flash-borrowed asset
-     * @param borrowedAmount The amount of the flash-borrowed asset
-     * @param premium The fee of the flash-borrowed asset
-     * @param initiator The address of the flashloan initiator
-     * @param params The byte-encoded params passed when initiating the flashloan
-     * @return True if the execution of the operation succeeds, false otherwise
-     */
-    function executeOperation(
-        address borrowedToken,
-        uint256 borrowedAmount,
-        uint256 premium,
-        address initiator,
-        bytes calldata params
-    ) external returns (bool) {
-        Params memory paramsDecoded = abi.decode(params, (Params));
-        swapRouter = ISwapRouter(paramsDecoded.swapRouter);
-        address token1 = paramsDecoded.token1;
-        address token2 = paramsDecoded.token2;
-
-        console.log("Borrowed amount");
-        console.log(borrowedAmount);
-        console.log("Premium");
-        console.log(premium);
-        console.log("Swap router");
-        console.log(paramsDecoded.swapRouter);
-        console.log("Token1");
-        console.log(token1);
-        console.log("Token2");
-        console.log(token2);
-
-        uint256 token1Amount = swapExactInputSingle(
-            borrowedToken,
-            token1,
-            borrowedAmount
-        );
-        uint256 token2Amount = swapExactInputSingle(
-            token1,
-            token2,
-            token1Amount
-        );
-        uint256 arbitragedAmount = swapExactInputSingle(
-            token2,
-            borrowedToken,
-            token2Amount
-        );
-
-        TransferHelper.safeApprove(
-            borrowedToken,
-            msg.sender,
-            borrowedAmount + premium
-        );
-
-        console.log("Arbitraged amount");
-        console.log(arbitragedAmount);
-
-        if (arbitragedAmount > borrowedAmount + premium) {
-            console.log("Profit");
-            console.log(arbitragedAmount - borrowedAmount - premium);
-        } else {
-            console.log("Loss");
-            console.log(borrowedAmount + premium - arbitragedAmount);
-        }
-
-        return true;
-    }
+    ...
 }
--- scripts/flash-loan.ts
+++ scripts/flash-loan.ts
@@ -1,71 +1,12 @@
 import { ethers, network } from "hardhat";
 import type { BigNumber } from "ethers";
 
-const impersonatedTokenTransfer = async ({
-  token,
-  from,
-  to,
-  amount,
-}: {
-  token: string;
-  from: string;
-  to: string;
-  amount: BigNumber;
-}) => {
-  await network.provider.request({
-    method: "hardhat_impersonateAccount",
-    params: [from],
-  });
-  const impersonatedSigner = await ethers.getSigner(from);
-  const tokenContract = await ethers.getContractAt("IERC20", token);
-  const txTransfer = await tokenContract
-    .connect(impersonatedSigner)
-    .transfer(to, amount);
-  await txTransfer.wait();
-  await network.provider.request({
-    method: "hardhat_stopImpersonatingAccount",
-    params: [from],
-  });
-};
+...
+  // Removed Aave references
 
-async function main() {
-  const poolAddressProviderAddress =
-    "0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb";
-  const arbitrumDAI = "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1";
-  const arbitrumDAItokenHolder = "0x0C249eF4592869a6bF8195d90de948BE2b7c2744";
-  const arbitrumUSDT = "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9";
-  const arbitrumUSDC = "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8";
-  const swapRouter = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
-  const amountToLend = ethers.utils.parseEther("1");
+  ...
 
-  const FlashLoan = await ethers.getContractFactory("FlashLoan");
-  const flashLoan = await FlashLoan.deploy();
-  await flashLoan.deployed();
-
-  const poolAddressProvider = await ethers.getContractAt(
-    "IPoolAddressesProvider",
-    poolAddressProviderAddress
-  );
-
-  const poolAddress = await poolAddressProvider.getPool();
-  const pool = await ethers.getContractAt("IPool", poolAddress);
-  const premium = await pool.FLASHLOAN_PREMIUM_TOTAL();
-  // FLASHLOAN_PREMIUM_TOTAL is expressed as a fraction of 10000
-  const premiumAmount = amountToLend.mul(premium).div(10000);
-
-  await impersonatedTokenTransfer({
-    token: arbitrumDAI,
-    from: arbitrumDAItokenHolder,
-    to: flashLoan.address,
-    amount: amountToLend.add(premiumAmount),
-  });
-
-  const paramsEncoded = ethers.utils.defaultAbiCoder.encode(
-    ["address", "address", "address"],
-    [swapRouter, arbitrumUSDT, arbitrumUSDC]
-  );
-
-  const txFlashLoan = await pool.flashLoanSimple(
+  const txFlashLoan = await exchange.flashLoanSimple(
     flashLoan.address,
     arbitrumDAI,
     amountToLend,
